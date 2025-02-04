da06129781b02ae27181bd9bef1d08069520dd80
Alexander Sumczynski <alexandermander123@gmail.com>
2025-02-04 13:04:53 +0100

    fix of env

--- goqlip/util/getEnv.go
+++ goqlip/util/getEnv.go
@@ -2,24 +2,9 @@ package util
 
 import (
 	"os"
-	"github.com/joho/godotenv"
 )
 
-func GetEnv(key string) string {
-	err := godotenv.Load("./config/.env")
-	if err != nil {
-		panic(err)
-	}
-
-	return os.Getenv(key)
-}
-
 func GetEnvCloud(key string) string {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-
 	return os.Getenv(key)
 }
 
